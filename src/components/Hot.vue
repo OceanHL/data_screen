e11c297d39a4d86bf632d4b2a3448b361a3daae3
OceanHL <876474290@qq.com>
2021-11-12 15:04:12 +0800

    热销商品占比（饼图）

---
+++ src/components/Hot.vue
@@ -0,0 +1,190 @@
+<!--
+ * @Author: jhl
+ * @Date: 2021-11-11 21:02:33
+ * @LastEditors: jhl
+ * @LastEditTime: 2021-11-12 14:53:18
+ * @Description: 
+-->
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="hot_ref"></div>
+    <span class="iconfont arr-left" @click="toLeft" :style="comStyle">&#xe6ef;</span>
+    <span class="iconfont arr-right" @click="toRight" :style="comStyle">&#xe6ed;</span>
+    <span class="cat-name">{{ catName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null, // 从服务器中获取的所有数据
+      currentIndex: 0, //
+      titleFontSize: 0, //
+    };
+  },
+  computed: {
+    catName() {
+      if (!this.allData) {
+        return "";
+      } else {
+        return this.allData[this.currentIndex].name;
+      }
+    },
+    comStyle() {
+      return {
+        fontSize: this.titleFontSize + "px",
+      };
+    },
+  },
+  mounted() {
+    this.initChart();
+    this.getData();
+    window.addEventListener("resize", this.screenAdapter);
+    this.screenAdapter();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.screenAdapter);
+  },
+  methods: {
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.hot_ref, "chalk");
+      const initOption = {
+        title: {
+          text: "▎热销商品的占比",
+          left: 20,
+          top: 20,
+        },
+        legend: {
+          top: "1 5%",
+          icon: "circle",
+        },
+        tooltip: {
+          show: true,
+          formatter: arg => {
+            const thirdCategory = arg.data.children;
+            const total = arg.data.children.reduce((pre, cur) => pre + cur.value, 0);
+            let retStr = "";
+            thirdCategory.forEach(item => {
+              retStr += `
+                ${item.name}: ${((item.value / total) * 100).toFixed(2) + "%"}
+                <br />
+              `;
+            });
+            return retStr;
+          },
+        },
+        series: [
+          {
+            type: "pie",
+            label: {
+              show: false, // 将文字隐藏起来
+            },
+            emphasis: {
+              label: {
+                show: true, // 鼠标移动到区域时，显示文字
+              },
+              labelLine: {
+                show: false, // 隐藏文本与区域之间的连线
+              },
+            },
+          },
+        ],
+      };
+      this.chartInstance.setOption(initOption);
+    },
+    async getData() {
+      const { data: resData } = await this.$http.get("hotproduct");
+      this.allData = resData;
+      console.log("resData", resData);
+      // 对 allData 进行赋值
+      this.updateChart();
+    },
+    updateChart() {
+      // 处理数据
+      // 处理图表需要的数据
+      const seriesData = this.allData[this.currentIndex].children.map(item => {
+        return {
+          name: item.name,
+          value: item.value,
+          children: item.children,
+        };
+      });
+      const updatOption = {
+        series: [
+          {
+            data: seriesData,
+          },
+        ],
+      };
+      this.chartInstance.setOption(updatOption);
+    },
+    screenAdapter() {
+      this.titleFontSize = (this.$refs.hot_ref.offsetWidth / 100) * 3.6;
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: this.titleFontSize,
+          },
+        },
+        legend: {
+          itemWidth: this.titleFontSize / 2,
+          itemHeight: this.titleFontSize / 2,
+          itemGap: this.titleFontSize / 2,
+          textStyle: {
+            fontSize: this.titleFontSize / 2,
+          },
+        },
+        series: [
+          {
+            radius: this.titleFontSize * 4.5,
+            center: ["50%", "50%"], // 饼图圆心的位置，元素1代表 x 轴，元素2代表 y 轴
+          },
+        ],
+      };
+      this.chartInstance.setOption(adapterOption);
+      this.chartInstance.resize();
+    },
+    toLeft() {
+      this.currentIndex--;
+      if (this.currentIndex < 0) {
+        this.currentIndex = this.allData.length - 1;
+      }
+      this.updateChart();
+    },
+    toRight() {
+      this.currentIndex++;
+      if (this.currentIndex > this.allData.length - 1) {
+        this.currentIndex = 0;
+      }
+      this.updateChart();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.arr-left {
+  position: absolute;
+  top: 50%;
+  left: 10%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  color: white;
+}
+.arr-right {
+  position: absolute;
+  top: 50%;
+  right: 10%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  color: white;
+}
+.cat-name {
+  position: absolute;
+  left: 80%;
+  bottom: 20%;
+  color: white;
+}
+</style>
